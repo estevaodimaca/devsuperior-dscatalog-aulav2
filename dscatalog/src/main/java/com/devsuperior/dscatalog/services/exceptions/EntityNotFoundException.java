1fd806bce825131a8903d4c833739aa31ab248f6
Estevao Pascoal Joao Dimaca <estevaodimaca@gmail.com>
2022-09-06 20:57:44 +0200

    Find category by, Id exeption handler

---
+++ dscatalog/src/main/java/com/devsuperior/dscatalog/services/exceptions/EntityNotFoundException.java
@@ -0,0 +1,16 @@
+package com.devsuperior.dscatalog.services.exceptions;
+
+public class EntityNotFoundException extends RuntimeException {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public EntityNotFoundException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+}
